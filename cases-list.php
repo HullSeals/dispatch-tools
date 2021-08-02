59627a9e85e8613af6c62cd1aa072df91c2c4489
Rixxan <rixxan@hullseals.space>
2021-08-01 22:50:13 -0400

    Fix SQL for cases list

--- cases-list.php
+++ cases-list.php
@@ -65,10 +65,11 @@ AS
 SELECT c.case_ID, client_nm, current_sys, platform_name, case_created, hs_kf,  COALESCE(seal_name, (SELECT seal_name FROM case_assigned WHERE case_stat != 8 AND (dispatch = TRUE AND support = FALSE AND c.case_ID = case_ID)), 'ERROR') AS seal_name
 FROM cases AS c
     JOIN lookups.platform_lu AS plu ON plu.platform_id = c.platform
-    JOIN case_assigned AS ca ON ca.case_ID = c.case_ID
+    LEFT JOIN case_assigned AS ca ON ca.case_ID = c.case_ID
     LEFT JOIN sealsCTI AS ss ON ss.seal_ID = ca.seal_kf_id
-    JOIN case_history AS ch ON ch.ch_id = c.last_ch_id
-    WHERE case_stat != 8 AND (dispatch = FALSE AND support = FALSE) GROUP BY c.case_ID");
+    LEFT JOIN case_history AS ch ON ch.ch_id = c.last_ch_id
+WHERE case_stat != 8
+GROUP BY c.case_ID");
 $stmt->execute();
 $result = $stmt->get_result();
 while ($row = $result->fetch_assoc()) {
